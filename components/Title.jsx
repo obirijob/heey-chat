9a6a42dec5b016bb049943669e9304bceb164aa8
Obiri Job <obirijob20@gmail.com>
2022-12-27 10:46:01 +0300

    Title

--- components/Title.jsx
+++ components/Title.jsx
@@ -2,12 +2,22 @@
 
 import { View } from 'moti'
 import React from 'react'
+import colors from '../constants/colors'
 import CustomText from './CustomText'
 
 const Title = ({ title }) => {
   return (
-    <View>
-      <CustomText style={{ fontSize: 30 }}>{title}</CustomText>
+    <View style={{ marginHorizontal: 20, marginVertical: 10 }}>
+      <CustomText
+        style={{
+          fontSize: 35,
+          fontFamily: 'msb',
+          color: colors.DARK_BLUE,
+          opacity: 0.8,
+        }}
+      >
+        {title}
+      </CustomText>
     </View>
   )
 }
